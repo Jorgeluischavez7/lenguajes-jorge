bb501e01c31b67ecf7f8cbf0f8b1af99f0f54e49
Jorgeluischavez7 <chavezj07@hotmail.com>
2015-01-19 19:34:17 -0600

    Update LenguajesJorge.java

--- src/lenguajes/jorge/LenguajesJorge.java
+++ src/lenguajes/jorge/LenguajesJorge.java
@@ -4,7 +4,8 @@ package lenguajes.jorge;
 public class LenguajesJorge {
     
     public static void main(String[] args) {
-        System.out.println("Mi primer programa de Java 2");
+        System.out.println("Linea modificada");
+        
     }
     
 }
