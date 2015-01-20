3d7026be291516670bfa55813d53af4f1f3b3408
T-107 <T-107@PC264>
2015-01-19 19:28:56 -0600

    Mi primer push a mi repositorio

---
+++ src/lenguajes/jorge/LenguajesJorge.java
@@ -0,0 +1,10 @@
+
+package lenguajes.jorge;
+
+public class LenguajesJorge {
+    
+    public static void main(String[] args) {
+        System.out.println("Mi primer programa de Java 2");
+    }
+    
+}
