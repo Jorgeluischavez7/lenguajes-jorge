37cc9d6f420a0e4d5975553fd73465819a67ba41
JorgeLuis <JorgeLuis@Familia_Chavez>
2015-02-05 16:25:43 -0600

    Tarea sobre el area del cuadrado, (Encapsulamiento).

---
+++ src/AreaCuadrado/TestArea.java
@@ -0,0 +1,18 @@
+
+package AreaCuadrado;
+
+/**
+ *
+ * @author JorgeLuis
+ */
+public class TestArea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cuadrado c=new Cuadrado();
+        c.setLado(4);
+        System.out.println("El area del cudrado es "+ c.calcularArea());
+    }
+}
